c2d5fecdac0148f9230e970c86eb7c71848e5a6e
sticky <sticky@fp>
2024-07-27 18:58:52 -0400

    feat: add login with privy

---
+++ src/app/components/LoginButton.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import {usePrivy} from '@privy-io/react-auth';
+
+export default function LoginButton() {
+  const {ready, authenticated, login, user} = usePrivy();
+  // Disable login when Privy is not ready or the user is already authenticated
+  const disableLogin = !ready || (ready && authenticated);
+
+  if (ready && !authenticated) {
+    // Replace this code with however you'd like to handle an unauthenticated user
+    // As an example, you might redirect them to a login page
+    return (
+        <button disabled={disableLogin} onClick={login}>
+            Log in
+        </button>
+    );
+    }
+
+  if (ready && authenticated) {
+    // Replace this code with however you'd like to handle an authenticated user
+    return <p>User {user?.id} is logged in.</p>;
+  }
+}
